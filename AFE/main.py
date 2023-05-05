1f11aa5da465e3dca63db900c3e000f2161cd1e5
AFE <57398351+AFE23-u@users.noreply.github.com>
2023-05-05 17:23:46 +0800

    Update main.py

--- AFE/main.py
+++ AFE/main.py
@@ -4,17 +4,29 @@ import shutil
 
 from infer import infer_from_config
 from train import train_from_config
+import argparse
 
 yn_CUP_CFG = "configs/yn_CUP.yml"
 yn_CUP_DIR = "yn_cup_res"
 
 
 def run_cup(cfg=yn_CUP_CFG,
-            log_dir=yn_CUP_DIR):
-    if os.path.exists(yn_CUP_DIR):
-        shutil.rmtree(yn_CUP_DIR)
-    train_from_config(cfg, log_dir)
-    infer_from_config(cfg, log_dir)
+            log_dir=yn_CUP_DIR, infer=False):
+    if not infer:
+        if os.path.exists(yn_CUP_DIR):
+            shutil.rmtree(yn_CUP_DIR)
+        train_from_config(cfg, log_dir)
+        infer_from_config(cfg, log_dir)
+    else:
+        infer_from_config(cfg, log_dir)
 
 
-run_cup(yn_CUP_CFG, yn_CUP_DIR)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='manual to this script')
+    parser.add_argument('--infer', type=str, default=None)
+    args = parser.parse_args()
+    if args.infer:
+        run_cup(yn_CUP_CFG, yn_CUP_DIR, True)
+    else:
+        run_cup(yn_CUP_CFG, yn_CUP_DIR)
+
